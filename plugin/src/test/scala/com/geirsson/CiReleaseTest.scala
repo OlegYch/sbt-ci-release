fb64a22e412db75b1682be3df0ccf4b8deff13a5
Eugene Yokota <eed3si9n@gmail.com>
2024-10-13 15:34:25 -0400

    Implement back publishing support
    **Problem**
    For plugin code, like sbt plugins and Scala compiler plugin,
    and even normal libraries, it's common for the maintainers to
    want to back publish the current code base against a new version
    of Scala (or sbt, Scala Nave etc) instead of bumping the version.
    
    The current "bump the version" approach effectively pushes the work
    of figuring out the correct patch version number to the end users.
    In addition, if the plugin or the library does not cross publish
    all variants, then the end user might need to figure out different
    patch version to use effectively the same code.
    
    **Solution**
    This implements a mini DSL for tag `version[@command|@3.3.4][#comment]`,
    which allows plugin authors to back publish a code base.

---
+++ plugin/src/test/scala/com/geirsson/CiReleaseTest.scala
@@ -0,0 +1,39 @@
+package com.geirsson
+
+import CiReleasePlugin.{ backPubVersionToCommand, dropBackPubCommand }
+
+class CiReleaseTest extends munit.FunSuite {
+  val expectedVer = "1.1.0"
+
+  test("Normal version default") {
+    assertEquals(backPubVersionToCommand("1.1.0"), "+publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0"), expectedVer)
+  }
+
+  test("Command starting with number is assumed to be a cross version") {
+    assertEquals(backPubVersionToCommand("1.1.0@2.12.20"), ";++2.12.20!;publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0@2.12.20"), expectedVer)
+
+    assertEquals(backPubVersionToCommand("1.1.0@3.x"), ";++3.x;publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0@3.x"), expectedVer)
+  }
+
+  test("Non-number is treated as an alternative publish command") {
+    assertEquals(backPubVersionToCommand("1.1.0@foo/publishSigned"), "foo/publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0@foo/publishSigned"), expectedVer)
+
+    assertEquals(backPubVersionToCommand("1.1.0@+foo/publishSigned"), "+foo/publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0@+foo/publishSigned"), expectedVer)
+  }
+
+  test("Treat # as comments") {
+    assertEquals(backPubVersionToCommand("1.1.0#comment"), "+publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0#comment"), expectedVer)
+
+    assertEquals(backPubVersionToCommand("1.1.0@2.12.20#comment"), ";++2.12.20!;publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0@2.12.20#comment"), expectedVer)
+
+    assertEquals(backPubVersionToCommand("1.1.0@3.x#comment"), ";++3.x;publishSigned")
+    assertEquals(dropBackPubCommand("1.1.0@3.x#comment"), expectedVer)
+  }
+}
